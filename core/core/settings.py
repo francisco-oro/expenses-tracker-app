798ae87777d8c7ff65fbbca61336dd9865765041
Francisco Abimael Oro Estrada <francisco.oro@comunidad.unam.mx>
2023-07-06 23:12:01 -0600

    docker-composer-2

--- core/core/settings.py
+++ core/core/settings.py
@@ -112,6 +112,7 @@ if DB_IS_AVAIL and POSTGRES_READY:
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
         }
     }
 
