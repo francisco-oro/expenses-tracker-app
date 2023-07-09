1522b6accb4b543f7ede8f5ff8c90349a9dbfd18
Francisco Abimael Oro Estrada <francisco.oro@comunidad.unam.mx>
2023-07-09 14:29:58 -0600

    password-reset-funcionality

--- core/authentication/views.py
+++ core/authentication/views.py
@@ -3,7 +3,7 @@ from django.views import View
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.core.mail import EmailMessage
-
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils.encoding import force_bytes,force_str, DjangoUnicodeDecodeError
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.sites.shortcuts import get_current_site
@@ -227,3 +227,136 @@ class LogoutView(View):
         logout(request)
         messages.success(request, "You have been logged out")
         return redirect('login')
+    
+
+class RequestPasswordResetEmail(View):
+    def get(self, request):
+        return render(request, 'authentication/reset-password.html')
+    
+    def post(self, request):
+        email = request.POST['email']
+
+        context = {
+            'values': request.POST
+        }
+
+        if not validate_email(email):
+            messages.error(request, "Please supply a valid email")
+            return render(request, 'authentication/reset-password.html')
+        
+        current_site= get_current_site(request)
+        user = User.objects.filter(email=email)
+
+        if user.exists():
+            email_contents = {
+                'user': user, 
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user[0].pk)),
+                'token': PasswordResetTokenGenerator().make_token(user[0])
+            }
+        
+            link = reverse('reset-user-password', kwargs={
+                'uidb64': email_contents['uid'],
+                'token':email_contents['token'],
+            })
+
+            email_subject = "Password Reset Instructions | Rumi Press"
+            
+            reset_url = 'http://' + current_site.domain + link
+
+            email = EmailMessage(
+                email_subject,
+                f"""Subject: Password Reset Instructions for Your Rumi Press Account 
+                                    
+                                    Dear {user[0].username}, 
+                                    
+                                    We have received a request to reset the password for your Rumi Press account. We understand that forgetting passwords can happen to the best of us, so don't worry – we're here to help you regain access to your account. 
+                                    
+                                    To reset your password, please follow the steps below: 
+                                    
+                                    1. Visit your unique reset password link at {reset_url}. 
+                                    2. Enter your new password
+                                    3. Keep it safe 
+
+                                    Please note that the password reset link is valid for 1 day. If you don't reset your password within this timeframe, you will need to initiate the process again. 
+                                    
+                                    If you did not request this password reset, please ignore this email. Rest assured that your account is still secure, and no changes have been made. 
+                                    
+                                    For any further assistance or queries, please feel free to reach out to our support team at [Support Email Address]. We'll be more than happy to assist you. 
+                                    
+                                    Thank you for choosing Rumi Press! 
+                                    
+                                    Best regards, 
+                                    
+                                    Francisco Oro
+                                    Rumi Press Support Team""",
+                'noreply@semicolon.com',
+                [email]
+            )
+            
+            email.send(fail_silently=False)
+
+            messages.success(request, 'We have sent you an email to reset your password')
+
+            return render(request, 'authentication/reset-password.html')
+
+class CompletePasswordResetView(View):
+    def get(self, request, uidb64, token):
+        context = {
+            'uidb64': uidb64,
+            'token': token
+        }
+
+        try:
+            user_id = force_str((urlsafe_base64_decode(uidb64)))
+            user = User.objects.get(pk = user_id)
+
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                messages.info(request, "Pasword reset link is invalid or expired. Please requesta a new one")
+                return redirect('reset-password')
+            
+
+        except Exception as indetifier:
+            messages.info(request, "Pasword reset link is invalid or expired. Please requesta a new one")
+            return redirect('reset-password')
+        
+        return render(request, 'authentication/set-new-password.html', context)
+    
+
+
+
+
+    def post(self, request, uidb64, token): 
+        context = {
+            'uidb64': uidb64,
+            'token': token
+        }
+
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.error(request, "Passwords don't match")
+            return render(request, 'authentication/set-new-password.html', context)
+
+        if len(password1) < 6:
+            messages.error(request, "Password is too short")
+            return render(request, 'authentication/set-new-password.html', context)
+        try:
+            user_id = force_str((urlsafe_base64_decode(uidb64)))
+            
+            user = User.objects.get(pk = user_id)
+            user.set_password(password1)
+            user.save()
+
+            messages.success(request, "Password reset succesfull. you can log in with your new password")
+            return redirect('login')
+        
+        except Exception as identifier:
+            messages.info(request, "Something went wrong")
+        return render(request, 'authentication/set-new-password.html', context)
+
+
+
+
+
