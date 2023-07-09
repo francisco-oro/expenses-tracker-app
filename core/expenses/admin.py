505ed5b6872c0b847af810f81aee0392836d683e
Francisco Abimael Oro Estrada <francisco.oro@comunidad.unam.mx>
2023-07-09 10:00:45 -0600

    admin-customization

--- core/expenses/admin.py
+++ core/expenses/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'date', 'description', 'owner', 'category',)
+    search_field = ('amount', 'date', 'description', 'owner', 'category',)
 
-admin.site.register(Expense)
+    list_per_page = 5
+
+admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
+
