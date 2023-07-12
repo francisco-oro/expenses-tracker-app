da8bfa54dfa72f968fe4aef4f532a1d1f6eb745c
Francisco Abimael Oro Estrada <francisco.oro@comunidad.unam.mx>
2023-07-12 03:32:09 -0600

    finished-deploy

---
+++ scripts/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -c
+
+python manage.py wait_for_db
+python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate
+
+uswgi --socket :9000 --workers 4 --master --enable-threads --module core.wsgi
